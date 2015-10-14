e5a2d73be163087cb15ba0d94f4ed5c0d406b823
Maxim Berezin <berezin.mvdev@yahoo.com>
2015-10-14 10:14:12 +0500

    index -> blink

--- js/blink.js
+++ js/blink.js
@@ -2,7 +2,6 @@
 var mraa = require("mraa");
 
 var led = new mraa.Gpio(4);
-var button = new mraa.Gpio(3)
 
 led.dir(mraa.DIR_OUT);
 
@@ -10,13 +9,6 @@ var state = true;
 
 function change() {
 	led.write(state ? 1 : 0);
-	state = !state;	
-}
-
-while (true) {
-
-	if (button.read() == 1) {
-		change();
-	}
-
+	state = !state;
+	setTimeout(change, 500);
 }
